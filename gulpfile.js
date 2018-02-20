6ab18579a37d05ad0a1b267ae883a8afcf27c7c8
rudePanda <maxwelladdicted@gmail.com>
2018-02-20 16:31:58 +0000

    Gulp copy not working
    Gulp copy not working

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,44 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     notify = require('gulp-notify'),
     livereload = require('gulp-livereload'),
+    gulpcopy = require('gulp-copy'),
     del = require('del');
 
-
+/*==========================================================
+============================================================  CLEAN
+============================================================
+*/
 gulp.task('clean', function() {
     return del(['dist/**/*']);
 });
 
+/*==========================================================
+============================================================  SCSS
+============================================================
+*/
 gulp.task('scss', function() {
   return sass('src/scss/app.scss', { style: 'expanded' })
     .pipe(gulp.dest('dist/assets/css'))
     .pipe(notify('SCSS task complete'));
 });
-
+/*==========================================================
+============================================================  COPY
+============================================================
+*/
 gulp.task('copy', ['scss'], function () {
-    return gulp.src(['src/**/*.html'], {
+    return gulp.src(['src/*.html'], {
         base: 'src'
     }).pipe(gulp.dest('dist'));
 });
-
 
 gulp.task('default', ['clean'], function() {
     gulp.start('copy');
 });
 
+/*==========================================================
+============================================================  WATCH
+============================================================
+*/
 gulp.task('watch', function() {
   // Watch .scss files
   gulp.watch('src/scss/**/*.scss', ['scss']);
